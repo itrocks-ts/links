0d5db48ed1a4f265c8191fbf63d9f2ecccf61b88
Baptiste Pillot <baptiste@pillot.fr>
2025-07-07 15:40:45 +0200

    v0.0.1

---
+++ README.md
@@ -0,0 +1,9 @@
+[![npm version](https://img.shields.io/npm/v/@itrocks/links?logo=npm)](https://www.npmjs.org/package/@itrocks/links)
+[![npm downloads](https://img.shields.io/npm/dm/@itrocks/links)](https://www.npmjs.org/package/@itrocks/links)
+[![GitHub](https://img.shields.io/github/last-commit/itrocks-ts/links?color=2dba4e&label=commit&logo=github)](https://github.com/itrocks-ts/links)
+[![issues](https://img.shields.io/github/issues/itrocks-ts/links)](https://github.com/itrocks-ts/links/issues)
+[![discord](https://img.shields.io/discord/1314141024020467782?color=7289da&label=discord&logo=discord&logoColor=white)](https://25.re/ditr)
+
+# links
+
+Multiple linked objects input and transformers
---
+++ src/front/links.ts
@@ -0,0 +1,71 @@
+
+function appendEmptyInput(container: HTMLUListElement, inputs: NodeListOf<HTMLInputElement>)
+{
+	const lastInput = inputs[inputs.length - 1]
+	if (!lastInput.parentElement || !lastInput.value.length) return
+
+	const li      = lastInput.parentElement.cloneNode(true) as HTMLLIElement
+	const idInput = li.querySelector<HTMLInputElement>('input[type=hidden]')
+	const input   = li.querySelector<HTMLInputElement>('input:not([type=hidden])')
+	if (!idInput || !input) return
+
+	idInput.removeAttribute('data-last-value')
+	idInput.setAttribute('value', '')
+	idInput.value = ''
+
+	input.classList.add('empty')
+	input.removeAttribute('data-last-value')
+	input.setAttribute('placeholder', '+')
+	input.setAttribute('value', '')
+	input.value = ''
+
+	let dotPosition = idInput.id.lastIndexOf('.')
+	idInput.setAttribute('id', dotPosition < 0 ? idInput.id : idInput.id.slice(0, dotPosition))
+	dotPosition = idInput.name.lastIndexOf('.')
+	idInput.setAttribute('name', dotPosition < 0 ? idInput.name : idInput.name.slice(0, dotPosition))
+	dotPosition = input.name.lastIndexOf('.')
+	input.setAttribute('name', dotPosition < 0 ? input.name : input.name.slice(0, dotPosition))
+
+	return container.appendChild(li)
+}
+
+function autoDim(event: Event)
+{
+	const input       = event.target as HTMLInputElement
+	const container   = input.closest('ul') as HTMLUListElement
+	const inputs      = container.querySelectorAll<HTMLInputElement>('input:not([type=hidden])')
+	const emptyInputs = updatePlaceholder(inputs)
+	if (!appendEmptyInput(container, inputs)) {
+		emptyInputs.pop()
+	}
+	removeEmptyInputs(emptyInputs)
+}
+
+export function links(input: HTMLInputElement)
+{
+	input.addEventListener('change', autoDim)
+	input.addEventListener('input', autoDim)
+}
+
+function removeEmptyInputs(emptyInputs: HTMLInputElement[])
+{
+	for (const input of emptyInputs) {
+		if (document.activeElement === input) continue
+		input.closest('li')?.remove()
+	}
+}
+
+function updatePlaceholder(inputs: NodeListOf<HTMLInputElement>)
+{
+	const emptyInputs: HTMLInputElement[] = []
+	inputs.forEach(input => {
+		if (input.value.length) {
+			input.removeAttribute('placeholder')
+		}
+		else {
+			emptyInputs.push(input)
+			input.setAttribute('placeholder', '+')
+		}
+	})
+	return emptyInputs
+}
